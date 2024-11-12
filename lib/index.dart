8c6e3b24b23dae6fb85a8ff44ddc4682720e6a4d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-12 09:20:29 +0000

    Updating to latest FlutterFlow output.

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,4 @@
 // Export pages
 export '/home_page/home_page_widget.dart' show HomePageWidget;
 export '/login_page/login_page_widget.dart' show LoginPageWidget;
+export '/test_page/test_page_widget.dart' show TestPageWidget;
