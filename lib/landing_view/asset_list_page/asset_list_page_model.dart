443e5cf79e73b7f0531aebe8d134fcd4d6dc8174
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-13 09:32:53 +0000

    Updating to latest FlutterFlow output.

--- lib/landing_view/asset_list_page/asset_list_page_model.dart
+++ lib/landing_view/asset_list_page/asset_list_page_model.dart
@@ -1,5 +1,7 @@
 import '/asset_view/asset_form_view/asset_form_view_widget.dart';
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
+import '/component/info_custom_view/info_custom_view_widget.dart';
 import '/flutter_flow/flutter_flow_choice_chips.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -7,6 +9,8 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'asset_list_page_widget.dart' show AssetListPageWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,6 +19,10 @@ import 'package:provider/provider.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
 
 class AssetListPageModel extends FlutterFlowModel<AssetListPageWidget> {
+  ///  Local state fields for this page.
+
+  String selectedStatus = 'ว่าง';
+
   ///  State fields for stateful widgets in this page.
 
   // State field(s) for ChoiceChips widget.
@@ -30,6 +38,8 @@ class AssetListPageModel extends FlutterFlowModel<AssetListPageWidget> {
   Query? listViewPagingQuery;
   List<StreamSubscription?> listViewStreamSubscriptions = [];
 
+  // Stores action output result for [Firestore Query - Query a collection] action in FloatingActionButton widget.
+  int? totalAsset;
   // Stores action output result for [Bottom Sheet - AssetFormView] action in FloatingActionButton widget.
   String? isUpdate;
 
