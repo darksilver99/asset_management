2ecb9d8665bd86706fba7fb07759eaf56d692b99
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-15 08:00:52 +0000

    Updating to latest FlutterFlow output.

--- lib/check_view/check_list_page/check_list_page_model.dart
+++ lib/check_view/check_list_page/check_list_page_model.dart
@@ -1,18 +1,31 @@
 import '/backend/backend.dart';
+import '/check_view/check_detail_view/check_detail_view_widget.dart';
+import '/check_view/check_form_view/check_form_view_widget.dart';
 import '/component/back_button_view/back_button_view_widget.dart';
+import '/component/no_data_view/no_data_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'check_list_page_widget.dart' show CheckListPageWidget;
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:provider/provider.dart';
+import 'package:webviewx_plus/webviewx_plus.dart';
 
 class CheckListPageModel extends FlutterFlowModel<CheckListPageWidget> {
   ///  State fields for stateful widgets in this page.
 
   // Model for BackButtonView component.
   late BackButtonViewModel backButtonViewModel;
+  // State field(s) for ListView widget.
+
+  PagingController<DocumentSnapshot?, CheckListRecord>?
+      listViewPagingController;
+  Query? listViewPagingQuery;
+  List<StreamSubscription?> listViewStreamSubscriptions = [];
 
   @override
   void initState(BuildContext context) {
@@ -22,5 +35,41 @@ class CheckListPageModel extends FlutterFlowModel<CheckListPageWidget> {
   @override
   void dispose() {
     backButtonViewModel.dispose();
+    listViewStreamSubscriptions.forEach((s) => s?.cancel());
+    listViewPagingController?.dispose();
+  }
+
+  /// Additional helper methods.
+  PagingController<DocumentSnapshot?, CheckListRecord> setListViewController(
+    Query query, {
+    DocumentReference<Object?>? parent,
+  }) {
+    listViewPagingController ??= _createListViewController(query, parent);
+    if (listViewPagingQuery != query) {
+      listViewPagingQuery = query;
+      listViewPagingController?.refresh();
+    }
+    return listViewPagingController!;
+  }
+
+  PagingController<DocumentSnapshot?, CheckListRecord>
+      _createListViewController(
+    Query query,
+    DocumentReference<Object?>? parent,
+  ) {
+    final controller = PagingController<DocumentSnapshot?, CheckListRecord>(
+        firstPageKey: null);
+    return controller
+      ..addPageRequestListener(
+        (nextPageMarker) => queryCheckListRecordPage(
+          parent: parent,
+          queryBuilder: (_) => listViewPagingQuery ??= query,
+          nextPageMarker: nextPageMarker,
+          streamSubscriptions: listViewStreamSubscriptions,
+          controller: controller,
+          pageSize: 10,
+          isStream: true,
+        ),
+      );
   }
 }
