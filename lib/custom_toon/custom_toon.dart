937cd26e13a686eb12f985a6b8b3bfb54dd8ae3a
darksilver99 <darksilver99@hotmail.com>
2024-11-15 14:07:51 +0700

    a

--- lib/custom_toon/custom_toon.dart
+++ lib/custom_toon/custom_toon.dart
@@ -1,25 +1,21 @@
+import 'dart:convert';
+
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:universal_io/io.dart';
 import 'package:widgets_to_image/widgets_to_image.dart';
 
-getSaveAndGetImagePath(WidgetsToImageController controller) async {
-  print("getSaveAndGetImagePath");
-
-  if (Platform.isAndroid) {
-    final deviceInfo = DeviceInfoPlugin();
-    final info = await deviceInfo.androidInfo;
-    if (int.parse(info.version.release) >= 13) {
-      var status = await Permission.storage.request();
-      if (status.isGranted) {
-        return getSaveAndGetImagePath(controller);
-      }
-    }
-  }
-
+Future<String?> getSaveAndGetImagePath(WidgetsToImageController controller) async {
   final bytes = await controller.capture();
-  final result = await ImageGallerySaver.saveImage(bytes!);
-  print(result["filePath"]);
+  String base64Image = base64Encode(bytes!);
+
+  final Directory tempDir = await getTemporaryDirectory();
+  final String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+
+  final String tempPath = '${tempDir.path}/qr_code_$timestamp.png';
+  await File(tempPath).writeAsBytes(base64Decode(base64Image));
+
+  return tempPath;
 }
